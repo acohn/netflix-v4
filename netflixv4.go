8890169e45f15e7334295d48f1abcbc394567254
Alex Cohn <alex@alexcohn.com>
2017-09-14 23:07:47 -0500

    add flags, sane exit

--- netflixv4.go
+++ netflixv4.go
@@ -1,70 +1,100 @@
 package main
 
+//This simple DNS client/server filters out ipv6 addresses from Netflix's domains.
+
 import (
 	"flag"
 	"github.com/miekg/dns"
 	"log"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 )
 
-var wg = &sync.WaitGroup{}
-
+//use one and only one dns client object
 var resolver = &dns.Client{}
 
+var wg = sync.WaitGroup{}
+
+//command-line flags
 var (
 	debugMode        bool
 	listenPort       uint
 	upstreamResolver string
 )
 
+//set up flags
 func init() {
 	flag.BoolVar(&debugMode, "d", false, "Print debug info")
 	flag.UintVar(&listenPort, "p", 5353, "Listen on this TCP/UDP port")
 	flag.StringVar(&upstreamResolver, "r", "8.8.8.8:53", "DNS resolver to proxy")
-
 }
 
 func main() {
 	flag.Parse()
+
 	serveMux := dns.NewServeMux()
-	serveMux.HandleFunc(".", handleNormalRequest)
+	serveMux.HandleFunc(".", handleNormalRequest) //if for some reason we get a non-Netflix dns request, handle it
 	serveMux.HandleFunc("netflix.com.", handleNetflixRequest)
 	serveMux.HandleFunc("nflxvideo.com.", handleNetflixRequest)
 	serveMux.HandleFunc("nflxext.com.", handleNetflixRequest)
 	serveMux.HandleFunc("nflximg.com.", handleNetflixRequest)
+
+	servers := make(chan *dns.Server, 2)
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		server := &dns.Server{Addr: "127.0.0.1:53535", Net: "udp", Handler: serveMux}
-		err := server.ListenAndServe()
-		if err != nil {
-			log.Printf("UDP goroutine err: %v", err)
+		server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp", Handler: serveMux}
+		servers <- server
+		if err := server.ListenAndServe(); err != nil {
+			log.Printf("UDP listener err: %v", err)
 		}
 	}()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		server := &dns.Server{Addr: "127.0.0.1:53535", Net: "tcp", Handler: serveMux}
-		err := server.ListenAndServe()
-		if err != nil {
-			log.Printf("TCP goroutine err: %v", err)
+		server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "tcp", Handler: serveMux}
+		servers <- server
+		if err := server.ListenAndServe(); err != nil {
+			log.Printf("TCP listener err: %v", err)
 		}
 	}()
 
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	_ = <-sig
+	for i := 0; i < 2; i++ {
+		server := <-servers
+		server.Shutdown()
+	}
 	wg.Wait()
 
 }
 
 func handleNormalRequest(writer dns.ResponseWriter, req *dns.Msg) {
+	if debugMode {
+		log.Printf("received non-Netflix query: %v", req)
+	}
 	response := fetchProxiedResult(req)
+	if debugMode {
+		log.Printf("sending response to non-Netflix query: %v", response)
+	}
 	writer.WriteMsg(response)
 }
 
 func handleNetflixRequest(writer dns.ResponseWriter, req *dns.Msg) {
+	if debugMode {
+		log.Printf("received Netflix query: %v", req)
+	}
 	response := fetchProxiedResult(req)
 	response.Answer = filterRRSet(response.Answer)
 	response.Extra = filterRRSet(response.Extra)
+	if debugMode {
+		log.Printf("sending response to Netflix query: %v", response)
+	}
 	writer.WriteMsg(response)
 }
 
@@ -88,7 +118,7 @@ func fetchProxiedResult(req *dns.Msg) *dns.Msg {
 	if debugMode {
 		log.Printf("sending nested query: %v", nestedQuery)
 	}
-	nestedResponse, _, err := resolver.Exchange(nestedQuery, "8.8.8.8:53")
+	nestedResponse, _, err := resolver.Exchange(nestedQuery, upstreamResolver)
 	if err != nil {
 		log.Print(err)
 		response := &dns.Msg{}
